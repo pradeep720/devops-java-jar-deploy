121c416ea58121a77ac4c9d7d54eea6e86ebb886
pradeep s <pradeepselventhiran01@gmail.com>
2021-08-13 13:23:57 +0530

    third commit

--- src/main/java/com/example/demo/web/HelloDevops.java
+++ src/main/java/com/example/demo/web/HelloDevops.java
@@ -10,7 +10,7 @@ public class HelloDevops {
 	@GetMapping
 	public String sayHello() {
 		
-		return "hello , i am java app spring boot project repository github deployed appservice using azure devops ci/cd  ,hello again ";
+		return "hello , i am java app spring boot project repository github deployed appservice using azure devops ci/cd  ,hello again ..";
 		
 	}
 
