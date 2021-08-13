fd10af25fa4c80357d79a9be990b047ed37b9918
pradeep s <pradeepselventhiran01@gmail.com>
2021-08-13 11:58:55 +0530

    first commit

---
+++ src/main/java/com/example/demo/AzDevopsJarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzDevopsJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzDevopsJarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/web/HelloDevops.java
@@ -0,0 +1,17 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloDevops {
+	
+	
+	@GetMapping
+	public String sayHello() {
+		
+		return "hello , i am java app spring boot project repository github deployed appservice using azure devops ci/cd ";
+		
+	}
+
+}
